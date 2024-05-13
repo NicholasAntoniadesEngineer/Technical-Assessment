b8e0c0bc29d5867b6b2acac8441d67ae1abeb4c7
Nicholas Antoniades <nickantoniades11@gmail.com>
2024-05-13 21:23:33 +0100

    Adding build files

---
+++ engineering_technical_assessment/build/CMakeFiles/AppMain.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AppMain"
+  "AppMain.pdb"
+  "CMakeFiles/AppMain.dir/source_files/ATSAMV71.cpp.o"
+  "CMakeFiles/AppMain.dir/source_files/ATSAMV71.cpp.o.d"
+  "CMakeFiles/AppMain.dir/source_files/app_main.cpp.o"
+  "CMakeFiles/AppMain.dir/source_files/app_main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AppMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
